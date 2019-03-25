b46260c1b1bd92fd4f3a33307336477688b8b7f1
baolilei <baolilei@100tal.com>
2019-03-25 19:34:40 +0800

    add sass loader

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App'
 import router from './router'
 import config from './config.js'
-import axios from 'axios'
+import './style/common.scss'
 import utils from './utils/index.js'
 
 Vue.config.productionTip = false
